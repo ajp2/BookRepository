263491d33949a9c999faf7cdddc24e9d125305e5
AJP <arvind.puvi@gmail.com>
2019-06-04 20:33:37 +1000

    Refactor class based components into functional components

--- BookRepository/ClientApp/src/App.js
+++ BookRepository/ClientApp/src/App.js
@@ -1,22 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import { Route } from "react-router";
-import { Layout } from "./components/Layout";
-import { Home } from "./components/Home";
-import { FetchData } from "./components/FetchData";
-import { Counter } from "./components/Counter";
-import Login from "./components/session/login";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import Login from "./components/session/Login";
+import Signup from "./components/session/Signup";
 
-export default class App extends Component {
-  static displayName = App.name;
-
-  render() {
-    return (
-      <Layout>
-        <Route exact path="/" component={Home} />
-        <Route path="/counter" component={Counter} />
-        <Route path="/fetch-data" component={FetchData} />
-        <Route path="/login" component={Login} />
-      </Layout>
-    );
-  }
+function App() {
+  return (
+    <Layout>
+      <Route exact path="/" component={Home} />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={Signup} />
+    </Layout>
+  );
 }
+
+export default App;
