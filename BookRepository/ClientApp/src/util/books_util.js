937bb8f42177fe2e19b8ea836b52a04f2234eae8
AJP <arvind.puvi@gmail.com>
2019-06-11 11:22:10 +1000

    Fix google books api 401 bug

--- BookRepository/ClientApp/src/util/books_util.js
+++ BookRepository/ClientApp/src/util/books_util.js
@@ -1,17 +1,15 @@
 import axios from "axios";
 
 export const searchBooks = query =>
-  axios
-    .get(
-      `https://www.googleapis.com/books/v1/volumes?q=${encodeURI(
-        query
-      )}&maxResults=25`
-    )
-    .then(res => res.data);
+  fetch(
+    `https://www.googleapis.com/books/v1/volumes?q=${encodeURI(
+      query
+    )}&maxResults=20`
+  ).then(res => res.json());
 
 export const getBooks = () => axios.get("/api/books").then(res => res.data);
 
 export const getBook = bookId =>
-  axios
-    .get(`https://www.googleapis.com/books/v1/volumes/${bookId}`)
-    .then(res => res.data);
+  fetch(`https://www.googleapis.com/books/v1/volumes/${bookId}`).then(res =>
+    res.json()
+  );
